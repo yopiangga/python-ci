0f4371ce1e21877a021539688c56d379df5a3699
yopiangga <yopigambyok@gmail.com>
2025-06-01 18:46:36 +0700

    init

---
+++ test_hello.py
@@ -0,0 +1,13 @@
+from hello import more_hello, more_goodbye
+
+
+def test_more_hello():
+    assert "hi" == more_hello()
+
+
+def test_more_hello_fail():
+    assert "hello" == more_hello()
+
+
+def test_more_goodbye():
+    assert "bye" == more_goodbye()
